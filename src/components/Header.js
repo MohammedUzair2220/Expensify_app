47c9c412cc84a433cb204eb145e01a58ce9f2b57
Uzair <uzairmohammed4@gmail.com>
2021-01-30 16:30:22 +0530

    setup production build setUp

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
     <header>
         <h1>Expansify</h1>
         <NavLink to="/" activeClassName="is-string" exact={true}>Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-string">Create Expanse</NavLink>
+        <NavLink to="/create" activeClassName="is-string">Add Expanse</NavLink>
         
     </header>
 )
