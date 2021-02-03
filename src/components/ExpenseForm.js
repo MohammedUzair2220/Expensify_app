e74b23b2babe72bd9cbc72c37ddc0d7014d0e9b7
Uzair <uzairmohammed4@gmail.com>
2021-02-03 13:08:53 +0530

    Text Change

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -12,7 +12,7 @@ export default class ExpenseForm extends React.Component {
 
       this.state = {
         description: props.expense ? props.expense.description : '',
-        note: props.expense ? props.expense.amount : '',
+        note: props.expense ? props.expense.note : '',
         amount: props.expense ? (props.expense.amount / 100).toString() : '',
         createdAt: props.expense ? moment(props.expense.createdAt) :moment(),
         calendarFocused: false,
@@ -57,6 +57,7 @@ export default class ExpenseForm extends React.Component {
         });
     }
   }
+
   render() {
     return (
         <form className="form" onSubmit= {this.onSubmit}>
